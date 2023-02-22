cf0c29fbb0770071e541b344d0b5e371ec72be7a
ravitejachikkala <79794643+ravitejachikkala@users.noreply.github.com>
2023-02-22 15:50:38 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
-  default = "us-east-1"
+  default = "ap-south-1"
 }
 
 variable "ami_id" {
-  type = "map"
-
+name = "jenkin-terra-instance"
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    ap-south-1    = "ami-0e742cca61fb65051"
+    instance_type          = "t2.micro"
+    key_name               = "DevopsVM-1"
+    
   }
 }
